112020b330b6fc75d94ec516bd6cf2063c9ff019
Ramiro Robles <ramisbaker@gmail.com>
2023-11-02 23:12:59 +0100

    audio e imagen

--- index.js
+++ index.js
@@ -9,4 +9,19 @@ function comprobar() {
 
     document.body.classList.add(check ? "bien" : "mal");
     document.body.classList.remove(!check ? "bien" : "mal");
+
+    if (check) {
+        document.getElementById('audio-ok').play();
+        document.getElementById('image-ok').classList.add('visible');
+        setTimeout(function() { 
+            document.getElementById('image-ok').classList.remove('visible');
+        } , 3000);
+    } else {
+        document.getElementById('audio-ko').play();
+        document.getElementById('image-ko').classList.add('visible');
+        setTimeout(function() { 
+            document.getElementById('image-ko').classList.remove('visible');
+        } , 3000);
+    }
 }
+
