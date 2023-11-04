de5d009642c0a219dd153f3e0a96c446f775717e
Ramiro Robles <ramisbaker@gmail.com>
2023-11-04 22:17:32 +0100

    mejoras menores

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 
-
 function comprobar() {
+    // Obtener valores
     var anterior = parseInt(document.getElementsByName("anterior")[0].value);
     var numero = parseInt(document.getElementsByName("numero")[0].value);
     var posterior = parseInt(document.getElementsByName("posterior")[0].value);
 
-    var check = ((anterior+1) == numero) && ((numero+1) == posterior);
+    // comprobar la corrección
+    var correcto = ((anterior+1) == numero) && ((numero+1) == posterior);
 
-    document.body.classList.add(check ? "bien" : "mal");
-    document.body.classList.remove(!check ? "bien" : "mal");
-
-    if (check) {
-        document.getElementById('audio-ok').play();
-        document.getElementById('image-ok').classList.add('visible');
-        setTimeout(function() { 
-            document.getElementById('image-ok').classList.remove('visible');
-        } , 3000);
-    } else {
-        document.getElementById('audio-ko').play();
-        document.getElementById('image-ko').classList.add('visible');
-        setTimeout(function() { 
-            document.getElementById('image-ko').classList.remove('visible');
-        } , 3000);
-    }
+    // reflejar el resultado en el aspecto
+    document.body.classList.add(correcto ? "bien" : "mal");
+    document.body.classList.remove(!correcto ? "bien" : "mal");
+    efectosResultado(correcto);
 }
 
+function efectosResultado(correcto) {
+    var sufijoId = correcto ? "ok" : "ko";
+
+    document.getElementById('audio-'+sufijoId).play();
+    document.getElementById('image-'+sufijoId).classList.add('visible');
+    setTimeout(function() { 
+        document.getElementById('image-'+sufijoId).classList.remove('visible');
+    } , 3000);
+}
